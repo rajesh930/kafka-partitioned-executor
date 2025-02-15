4c01a5615f8727c3107aed5cdf3e0087fdf7b67e
Rajesh Kumar <rajesh930@gmail.com>
2025-02-15 15:13:20 +0400

    Initial commit

---
+++ kafka-example-consumer/src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * @author rajesh
+ * @since 15/02/25 15:07
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
